cd64f23164b422dc39b629e28ca064ce6a0b9058
shabrina0401 <85988307+shabrina0401@users.noreply.github.com>
2022-10-30 17:11:53 +0700

    Create vignette.js

---
+++ Ads/vignette.js
@@ -0,0 +1,1 @@
+    <script>(function(d,z,s){s.src='https://'+d+'/401/'+z;try{(document.body||document.documentElement).appendChild(s)}catch(e){}})('oaphoace.net',4945080,document.createElement('script'))</script>
