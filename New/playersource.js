91a220ef4b1d8a0d04e4f5e9c9bf9140cf523958
shabrina0401 <85988307+shabrina0401@users.noreply.github.com>
2023-02-28 08:15:01 +0700

    Create playersource.js

---
+++ New/playersource.js
@@ -0,0 +1,44 @@
+function playM3u8(url, license){                        
+ var player = new Clappr.Player({                 
+                    source: url,
+                    parentId: '#player',
+                    preload: 'auto',
+                    autoPlay: 'false',
+                    width: '100%',
+                    height: '100%',
+                    fullscreenEnabled:'true',
+                    hideMediaControl: 'false',
+                    watermark: 'https://raw.githubusercontent.com/shabrina0401/pict/main/Mcqueen.png',
+                    position: 'bottom-right',
+                    plugins: [LevelSelector, ChromecastPlugin, ClapprPip.PipButton, ClapprPip.PipPlugin, DashShakaPlayback],
+           
+                    chromecast: {
+                      appId: '9DFB77C0',
+                      media: {
+                        type: ChromecastPlugin.Movie,
+                        title: 'Player HLS - DASH',
+                      },
+                  },
+
+                    shakaConfiguration: {
+                    preferredAudioLanguage: 'en-US',
+                    drm: {
+                    servers: {
+                    'com.widevine.alpha': 'https://drmwidevine.nontonbola.my.id/https://mrpw.ptmnc01.verspective.net/?deviceId=YzU1NDI4MzgtODNkNC0zZGQwLWJhYjktOGYyYTJjOTAzMWVj'
+                             }
+                         },
+                    streaming: {
+                      rebufferingGoal: 15
+                    }
+                  },
+                  shakaOnBeforeLoad: function(shaka_player) {
+                    // shaka_player.getNetworkingEngine().registerRequestFilter() ...
+                  },
+
+                });
+
+  document.title = "Watching: " +url;
+    
+}
+
+playM3u8(window.location.href.split("#")[1], window.location.href.split("=")[1])
